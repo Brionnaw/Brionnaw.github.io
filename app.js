cff37a0f406d990e0d5d1b7c05d7dca7058ed199
Brionna White <Brionnanwhite@gmail.com>
2016-11-04 16:45:06 -0700

    add dropdown

---
+++ app.js
@@ -0,0 +1,8 @@
+var clickDropDown = function(){
+  var dropdown = document.getElementById("dropdown")
+  var dropdownContent = document.getElementById("dropdownContent")
+    dropdown.style.display = "block"
+    dropdownContent.style.display = "block"
+
+}
+console.log('hit')
