6b85339599db6e4be7cf0e460b2f9a7dd8d300eb
Brionna White <Brionnanwhite@gmail.com>
2016-11-06 13:05:06 -0800

    toggle

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// TOGGLE FUNCTION FOR NAV BAR ICON 
 function togglevisibility(id) {
-var e = document.getElementById(id);
-if(e.style.display == 'none'){
-e.style.display = 'block';
-}else{
-e.style.display = 'none';
-}
+  var e = document.getElementById(id);
+  if(e.style.display == 'none'){
+    e.style.display = 'block';
+  }else{
+    e.style.display = 'none';
+  }
 }
