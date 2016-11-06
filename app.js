d7d9160f7059d3b11a64bc30c75adb525d1d46a0
Brionna White <Brionnanwhite@gmail.com>
2016-11-06 12:51:32 -0800

    toggle

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-var toggleMenu = function(){
-  var dropdown = document.getElementById("dropdown")
-  var dropdownContent = document.getElementById("dropdownContent")
-  if (dropdown.style.display == "block")
-  dropdown.style.display = "none";
-  } else {
-    dropdown.style.display = "none";
-    if(dropdownContent.style.display == "block")
-    dropdownContent.style.display = "none";
-  }else {
-    dropdownContent.style.display = "none";
-  }
+function togglevisibility(id) {
+var e = document.getElementById(id);
+if(e.style.display == 'none'){
+e.style.display = 'block';
+}else{
+e.style.display = 'none';
+}
 }
